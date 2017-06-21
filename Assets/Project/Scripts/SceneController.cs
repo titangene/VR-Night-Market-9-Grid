7553b9096063d5f350c76b8007d21f8ddce6d33d
titangene <titangene01@gmail.com>
2017-06-21 23:20:45 +0800

    Add Home sence goto  Game sence

---
+++ Assets/Project/Scripts/SceneController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+	public void GoHome() {
+		SceneManager.LoadScene("Home");
+	}
+
+	public void RestartGame() {
+		SceneManager.LoadScene("Game");
+	}
+
+	public void PlayGame() {
+		SceneManager.LoadScene("Game");
+	}
+}
