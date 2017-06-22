65b254153cc93f1a5f2c6629219689c41f22ae96
titangene <titangene01@gmail.com>
2017-06-22 13:38:30 +0800

    Refactor Script code
    - BulletDestory
    - BulletSpawn

--- Assets/Project/Scripts/BallDestory.cs
+++ Assets/Project/Scripts/BallDestory.cs
@@ -1,8 +1,17 @@
 ﻿using UnityEngine;
 
-public class BulletDestory : MonoBehaviour {
+public class BallDestory : MonoBehaviour {
     private void OnTriggerEnter(Collider other) {
-        Destroy(other.gameObject);  // 刪除碰觸到範圍外的球
-        GameController.Instance.GameOver();
+        if (IsBall(other)) {
+            Destroy(other.gameObject);  // 刪除碰觸到範圍外的球
+            GameController.Instance.GameOver();
+        }
+    }
+
+    /// <summary>
+    /// 球是否碰觸到範圍外
+    /// </summary>
+    private bool IsBall(Collider other) {
+        return other.tag == "Ball";
     }
 }
--- Assets/Project/Scripts/BallSpawn.cs
+++ Assets/Project/Scripts/BallSpawn.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class BulletSpawn : MonoBehaviour {
+public class BallSpawn : MonoBehaviour {
     public float Throw_Power = 8.0f;    // ran.Next(6, 12)
     public System.Random ran = new System.Random();
     private Rigidbody HitRB;
@@ -20,8 +20,8 @@ public class BulletSpawn : MonoBehaviour {
     void ShootBall() {
         // 按 Cardboard 按鈕時，如果還有剩球 -> 發射球
         if (GameController.Instance.ball > 0 && Input.GetMouseButtonDown(0)) {
-            GameObject obj = Instantiate(GameController.Instance.BallObj, transform.position, transform.rotation);
-            HitRB = obj.GetComponent<Rigidbody>();
+            GameObject BallObj = Instantiate(GameController.Instance.BallObj, transform.position, transform.rotation);
+            HitRB = BallObj.GetComponent<Rigidbody>();
             HitRB.velocity = Camera.main.transform.forward * Throw_Power;
             GameController.Instance.UpdateBall();
         }
